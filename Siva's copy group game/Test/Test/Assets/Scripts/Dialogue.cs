9edb5087105a4c5df2d3adf425c5932347d56be0
BannaMann <likesred2@gmail.com>
2023-09-06 00:19:34 -0400

    Dialogue controls and localization
    Fill out localization tables, add Input System plugin and keyboard controls for dialogue menu, refactor

--- Siva's copy group game/Test/Test/Assets/Scripts/Dialogue.cs
+++ Siva's copy group game/Test/Test/Assets/Scripts/Dialogue.cs
@@ -2,57 +2,120 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.Localization;
 using System;
+using System.Collections.Generic;
 
 public class Dialogue: MonoBehaviour
 {
-    VisualElement root;
+    static VisualElement root;
+    static Dialogue instance;
+
     public LocalizedString test;
     public LocalizedAudioClip clip;
-    public bool PLAY_AUDIO_ON_HOVER;
     public AudioSource PlayerAudioSource;
+    public bool PLAY_AUDIO_ON_HOVER;
+
+    private static List<DialogueButton> ResponseChoices = new List<DialogueButton>();
+    private static int ResponseHoveredIndex;
+
+    static void HoverResponse(int index)
+    {
+        ResponseHoveredIndex = index;
+        ResponseChoices[index].OnHover();
+    }
 
     private void OnEnable()
     {
+        instance = this;
+
         var uiDocument = GetComponent<UIDocument>();
 
         root = uiDocument.rootVisualElement;
-        AddPlayerResponse(test, clip, () => Debug.Log("e"));
+        AddPlayerResponse(test, clip, () => AddNpcDialogue(test));
+        AddPlayerResponse(test, clip, () => AddNpcDialogue(test));
     }
 
-    public void AddNpcDialogue(LocalizedString text)
+    public static void AddNpcDialogue(LocalizedString text)
     {
+        // Opens dialogue UI, clears existing player response choices, and sets NPC dialogue
+
+        // Open menu
+        instance.gameObject.SetActive(true);
+
         // Set text
         Label npcText = root.Q("npc-dialogue") as Label;
         npcText.text = text.GetLocalizedString();
 
         // Delete response choice buttons
-        root.Query("response-choice").ForEach(i => i.Remove(i));
+        root.Query(classes:"response-choice").ForEach(button => root.Remove(button));
+        ResponseChoices.Clear();
     }
 
-    public void AddPlayerResponse(LocalizedString text, LocalizedAudioClip audio, Action whenChosen)
+    public static void AddPlayerResponse(LocalizedString text, LocalizedAudioClip audio, Action whenChosen)
     {
-        // TODO does this order work
-        // Make button
-        Button button = new Button();
-        root.Add(button);
-        button.text = text.GetLocalizedString();
-        button.clicked += whenChosen;
-        button.name = "response-choice";
+        root.Add(new DialogueButton(text, audio, whenChosen));
+    }
 
-        // When hovered, if PLAY_AUDIO_ON_HOVER is true, play audio
-        button.RegisterCallback<MouseOverEvent>(ignored =>
+    private class DialogueButton : Button
+    {
+        private int index;
+        private LocalizedString localizedText;
+        public LocalizedAudioClip localizedAudio;
+        private Action whenChosen;
+
+        public DialogueButton(LocalizedString text, LocalizedAudioClip audio, Action whenChosen)
         {
-            if (PLAY_AUDIO_ON_HOVER)
+            this.index = ResponseChoices.Count;
+            ResponseChoices.Add(this);
+            localizedText = text;
+            localizedAudio = audio;
+            this.clicked += whenChosen;
+
+            this.AddToClassList("response-choice");
+
+            this.RegisterCallback<MouseOverEvent>(i => HoverResponse(index));
+        }
+
+        public void OnHover()
+        {
+            if (instance.PLAY_AUDIO_ON_HOVER)
             {
-                // TODO retrieve audio from localizedasset
-                // PlayerAudioSource.clip = audio;
-                // PlayerAudioSource.Play();
+                instance.PlayerAudioSource.clip = localizedAudio.LoadAsset();
+                instance.PlayerAudioSource.Play();
             }
-        });
+            Debug.Log(index);
+        }
     }
 
-    private void OnDisable()
+    public static void CloseMenu()
     {
-        // _button.UnregisterCallback<ClickEvent>(PrintClickMessage);
+        // Closes dialogue UI, and stops audio from all participants in the conversation
+
+        // Close UI
+        if (!instance.gameObject.activeSelf)
+        {
+            Debug.LogWarning("Attempted to close dialogue UI when it was already closed. Or maybe not; this warning appears at wrong times sometimes");
+        }
+        instance.gameObject.SetActive(false);
+
+        // Stop audio from all interlocutors
+        instance.PlayerAudioSource.Stop();
+        // TODO add NPC audio
+    }
+
+    // TODO something wrong around here; the inputs don't work like they should
+    public static void MoveSelectorUp()
+    {
+        if (ResponseHoveredIndex > 0)
+        {
+            HoverResponse(ResponseHoveredIndex - 1);
+        }
+    }
+
+    public static void MoveSelectorDown()
+    {
+        if (ResponseHoveredIndex < ResponseHoveredIndex - 1)
+        {
+            HoverResponse(ResponseHoveredIndex + 1);
+        }
     }
 }
